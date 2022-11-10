279ff9535daa497a7461ace75b4931754c4be89c
MariaMihajlenko <Mariamihajlenko3117@gmail.com>
2022-11-10 19:57:04 +0500

    hw9

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from kivy.app import App
-
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
-
 from kivy.uix.button import Button
 from kivy.config import Config
 
