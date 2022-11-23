1d3b07e397329a6a2e2b98fce9a8c68c996f41c0
MariaMihajlenko <Mariamihajlenko3117@gmail.com>
2022-11-23 12:41:14 +0500

    9_1

--- HW_10/hw_10_main_2.py
+++ HW_10/hw_10_main_2.py
@@ -9,7 +9,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from sympy import symbols
 from sympy.parsing.sympy_parser import parse_expr
 
-API_TOKEN = '5677382019:AAEHHOFd7Z_8AfPUSeZ3XMHC_oa_W1wySlc'
+API_TOKEN = '******'
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -27,22 +27,22 @@ async def send_welcome(message: types.Message):
     await message.reply("Hi!\nI'm Bot!\n")
 
 
-@dp.message_handler(commands=['mn'])
-async def mn(message: types.Message):
-    expression = [message]
+@dp.message_handler(commands=['sum'])
+async def command_sum(message: types.Message):
+    #expression =input(message.text)
+    expression = '-2+x^1-3*x^2+x^2+100*x^3-2*x'
     x = symbols('x')
     formula = parse_expr(expression.replace('^', '**'), local_dict={'x': x})
-    await expression.reply('Формула:', formula)
+    await bot.send_message(message.chat.id,formula )
     for i in range(3):
-        y = formula.subs(x, i)
-        await expression.reply(f'x={i}', f'y={y}')
-
+       y = formula.subs(x, i)
+       await bot.send_message(message.chat.id,(f'x={i}', f'y={y}'))
 @dp.message_handler()
 async def echo(message: types.Message):
     # old style:
     # await bot.send_message(message.chat.id, message.text)
 
-    await message.answer(message.text)      
+    await message.answer(message.text)     
 
 
 if __name__ == '__main__':
